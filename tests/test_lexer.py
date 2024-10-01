8d521cac4f0111ee2122adef7f0801b490c943e4
Daniel Barbosa <danielolibar@gmail.com>
2024-10-01 17:02:58 -0300

    test(lexer): add tests for division operator input handling

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -207,5 +207,20 @@ class TestLexer(unittest.TestCase):
         self.assertEqual(token2.text, ' ')
         self.assertEqual(token3.text, '*')
 
+    def testDivisionInputNumberOfTokens(self):
+        lexer = Lexer('/')
+        tokens = list(lexer.tokens())
+        self.assertEqual(len(tokens), 1)
+
+    def testDivisionInputTokenType(self):
+        lexer = Lexer('/')
+        token = lexer.getToken()
+        self.assertEqual(token.kind, TokenType.DIV)
+
+    def testDivisionInputText(self):
+        lexer = Lexer('/')
+        token = lexer.getToken()
+        self.assertEqual(token.text, '/')
+
 if __name__ == "__main__":
     pass
