2fd50cc7e233ef976d7cb245d36a78fd10212c26
Daniel Barbosa <danielolibar@gmail.com>
2024-11-06 09:58:14 -0300

    feat(`Parser.py`): implement the parser

--- Parser.py
+++ Parser.py
@@ -166,4 +166,83 @@ class Parser:
         boolean_literal         ::= 'true' 
                                 | 'false'
         """
-        return None
+        return self.bool_expression()
+    
+    def advance(self):
+        if self.cur_token_idx < len(self.tokens) - 1:
+            self.cur_token_idx += 1
+
+    def current_token(self):
+        return self.tokens[self.cur_token_idx]
+
+    def match(self, token_type):
+        if self.current_token().kind == token_type:
+            self.advance()
+            return True
+        return False
+
+    def bool_expression(self):
+        if self.match(TokenType.NOT):
+            return Not(self.bool_expression())
+        else:
+            return self.comparison_expression()
+
+    def comparison_expression(self):
+        left = self.arithmetic_expression()
+        
+        if self.match(TokenType.EQL):
+            right = self.arithmetic_expression()
+            return Eql(left, right)
+        elif self.match(TokenType.LTH):
+            right = self.arithmetic_expression()
+            return Lth(left, right)
+        elif self.match(TokenType.LEQ):
+            right = self.arithmetic_expression()
+            return Leq(left, right)
+        
+        return left
+
+    def arithmetic_expression(self):
+        left = self.term()
+        
+        while self.current_token().kind in (TokenType.ADD, TokenType.SUB):
+            if self.match(TokenType.ADD):
+                right = self.term()
+                left = Add(left, right)
+            elif self.match(TokenType.SUB):
+                right = self.term()
+                left = Sub(left, right)
+        
+        return left
+
+    def term(self):
+        left = self.factor()
+        
+        while self.current_token().kind in (TokenType.MUL, TokenType.DIV):
+            if self.match(TokenType.MUL):
+                right = self.factor()
+                left = Mul(left, right)
+            elif self.match(TokenType.DIV):
+                right = self.factor()
+                left = Div(left, right)
+        
+        return left
+
+    def factor(self):
+        if self.match(TokenType.NEG):
+            return Neg(self.factor())
+        elif self.match(TokenType.LPR):
+            expr = self.bool_expression()
+            if not self.match(TokenType.RPR):
+                raise ValueError("Expected closing parenthesis")
+            return expr
+        elif self.current_token().kind == TokenType.NUM:
+            num_value = int(self.current_token().text)
+            self.advance()
+            return Num(num_value)
+        elif self.match(TokenType.TRU):
+            return Bln(True)
+        elif self.match(TokenType.FLS):
+            return Bln(False)
+        else:
+            raise ValueError("Unexpected token: " + self.current_token().text)
