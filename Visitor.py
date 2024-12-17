8d9ecb8f44961e0f8ac3c74c52189ed37ba17aaa
Daniel Barbosa <danielolibar@gmail.com>
2024-12-17 00:51:07 -0300

    feat(`Visitor`): implement GenVisitor for code generation
    - Also deleted other visitors

--- Visitor.py
+++ Visitor.py
@@ -1,6 +1,7 @@
 import sys
 from abc import ABC, abstractmethod
 from Expression import *
+import Asm as AsmModule
 
 
 class Visitor(ABC):
@@ -27,14 +28,6 @@ class Visitor(ABC):
 
     @abstractmethod
     def visit_eql(self, exp, arg):
-        pass
-
-    @abstractmethod
-    def visit_and(self, exp, arg):
-        pass
-
-    @abstractmethod
-    def visit_or(self, exp, arg):
         pass
 
     @abstractmethod
@@ -73,349 +66,103 @@ class Visitor(ABC):
     def visit_let(self, exp, arg):
         pass
 
-    @abstractmethod
-    def visit_ifThenElse(self, exp, arg):
-        pass
 
-    @abstractmethod
-    def visit_fn(self, exp, arg):
-        pass
+class GenVisitor(Visitor):
+    def __init__(self):
+        self.label_counter = 0
 
-    @abstractmethod
-    def visit_app(self, exp, arg):
-        pass
+    def new_var(self):
+        self.label_counter += 1
+        return f"v{self.label_counter}"
 
+    def visit_var(self, exp, prog):
+        return exp.identifier
 
-class ArrowType:
-    """
-    This is the class that represents function types. A function type is just
-    that: the type of a function. Function types are also called "arrow types",
-    because they tend to be represented as 't0 -> t1'. Thus, an arrow type
-    has two parts: the head type (t0) and the tail type (t1).
+    def visit_bln(self, exp, prog):
+        reg = self.new_var()
+        val = 1 if exp.bln else 0
+        prog.add_inst(AsmModule.Addi(reg, "x0", val))
+        return reg
 
-    Usage:
-        >>> t = ArrowType(type(1), type(True))
-        >>> t.hd == type(1) and t.tl == type(True)
-        True
+    def visit_num(self, exp, prog):
+        reg = self.new_var()
+        prog.add_inst(AsmModule.Addi(reg, "x0", exp.num))
+        return reg
 
-        >>> t = ArrowType(type(1), type(True))
-        >>> str(t)
-        "<class 'int'> -> <class 'bool'>"
-    """
+    def compute_equality(self, lhs_reg, rhs_reg, prog):
+        delta = self.new_var()
+        cond1 = self.new_var()
+        cond2 = self.new_var()
+        equal_reg = self.new_var()
 
-    def __init__(self, tp_formal, tp_body):
-        self.hd = tp_formal
-        self.tl = tp_body
+        prog.add_inst(AsmModule.Sub(delta, lhs_reg, rhs_reg))
+        prog.add_inst(AsmModule.Slti(cond1, delta, 1))
+        prog.add_inst(AsmModule.Slti(cond2, delta, 0))
+        prog.add_inst(AsmModule.Xor(equal_reg, cond1, cond2))
+        return equal_reg
 
-    def __eq__(self, other):
-        """
-        Two arrow types are equal if their head and tail are equal.
-        Example:
-            >>> t0 = ArrowType(type(1), ArrowType(type(1), type(1)))
-            >>> t1 = ArrowType(type(1), ArrowType(type(1), type(1)))
-            >>> t0 == t1
-            True
-        """
-        if isinstance(other, ArrowType):
-            return self.hd == other.hd and self.tl == other.tl
-        else:
-            return False
+    def visit_eql(self, exp, prog):
+        lhs = exp.left.accept(self, prog)
+        rhs = exp.right.accept(self, prog)
+        return self.compute_equality(lhs, rhs, prog)
 
-    def __repr__(self):
-        if isinstance(self.hd, ArrowType):
-            hd_str = f"( {str(self.hd)} )"
-        else:
-            hd_str = str(self.hd)
-        if isinstance(self.tl, ArrowType):
-            tl_str = f"( {str(self.tl)} )"
-        else:
-            tl_str = str(self.tl)
-        return f"{hd_str} -> {tl_str}"
+    def visit_add(self, exp, prog):
+        lhs = exp.left.accept(self, prog)
+        rhs = exp.right.accept(self, prog)
+        result_reg = self.new_var()
+        prog.add_inst(AsmModule.Add(result_reg, lhs, rhs))
+        return result_reg
 
+    def visit_sub(self, exp, prog):
+        lhs = exp.left.accept(self, prog)
+        rhs = exp.right.accept(self, prog)
+        result_reg = self.new_var()
+        prog.add_inst(AsmModule.Sub(result_reg, lhs, rhs))
+        return result_reg
 
-class TypeCheckVisitor(Visitor):
-    """
-    The TypeCheckVisitor class evaluates logical and arithmetic expressions. The
-    result of evaluating an expression is the value of that expression. The
-    inherited attribute propagated throughout visits is the environment that
-    associates the names of variables with values.
-    """
+    def visit_mul(self, exp, prog):
+        lhs = exp.left.accept(self, prog)
+        rhs = exp.right.accept(self, prog)
+        result_reg = self.new_var()
+        prog.add_inst(AsmModule.Mul(result_reg, lhs, rhs))
+        return result_reg
 
-    def visit_var(self, exp, env):
-        """
-        Usage:
-            >>> e = Var('t')
-            >>> ev = TypeCheckVisitor()
-            >>> e.accept(ev, {'t':type(1)})
-            <class 'int'>
+    def visit_div(self, exp, prog):
+        lhs = exp.left.accept(self, prog)
+        rhs = exp.right.accept(self, prog)
+        result_reg = self.new_var()
+        prog.add_inst(AsmModule.Div(result_reg, lhs, rhs))
+        return result_reg
 
-            >>> e = Var('t')
-            >>> ev = TypeCheckVisitor()
-            >>> e.accept(ev, {'t':ArrowType(type(1), type(True))})
-            <class 'int'> -> <class 'bool'>
-        """
-        if exp.identifier in env:
-            return env[exp.identifier]
-        else:
-            sys.exit("Def error")
+    def visit_lth(self, exp, prog):
+        lhs = exp.left.accept(self, prog)
+        rhs = exp.right.accept(self, prog)
+        result_reg = self.new_var()
+        prog.add_inst(AsmModule.Slt(result_reg, lhs, rhs))
+        return result_reg
 
-    def visit_bln(self, exp, env):
-        """
-        Usage:
-            >>> e = Bln(True)
-            >>> ev = TypeCheckVisitor()
-            >>> e.accept(ev, None)
-            <class 'bool'>
-        """
-        return type(exp.bln)
+    def visit_leq(self, exp, prog):
+        lhs = exp.left.accept(self, prog)
+        rhs = exp.right.accept(self, prog)
+        less_reg = self.new_var()
+        eq_reg = self.compute_equality(lhs, rhs, prog)
+        prog.add_inst(AsmModule.Slt(less_reg, lhs, rhs))
+        leq_reg = self.new_var()
+        prog.add_inst(AsmModule.Add(leq_reg, less_reg, eq_reg))
+        return leq_reg
 
-    def visit_num(self, exp, env):
-        """
-        Usage:
-            >>> e = Num(1)
-            >>> ev = TypeCheckVisitor()
-            >>> e.accept(ev, None)
-            <class 'int'>
-        """
-        return type(exp.num)
+    def visit_neg(self, exp, prog):
+        value_reg = exp.exp.accept(self, prog)
+        result_reg = self.new_var()
+        prog.add_inst(AsmModule.Sub(result_reg, "x0", value_reg))
+        return result_reg
 
-    def visit_eql(self, exp, env): # Implemented for you :)
-        """
-        Usage:
-            >>> e = Eql(Num(1), Num(2))
-            >>> ev = TypeCheckVisitor()
-            >>> e.accept(ev, None)
-            <class 'bool'>
-        """
-        if exp.left.accept(self, env) == exp.right.accept(self, env):
-            return type(True)
-        else:
-            sys.exit("Type error")
+    def visit_not(self, exp, prog):
+        value_reg = exp.exp.accept(self, prog)
+        return self.compute_equality(value_reg, "x0", prog)
 
-    def visit_and(self, exp, env):
-        """
-        Usage:
-            >>> e = And(Bln(True), Bln(False))
-            >>> ev = TypeCheckVisitor()
-            >>> e.accept(ev, None)
-            <class 'bool'>
-        """
-        if exp.left.accept(self, env) == exp.right.accept(self, env) == type(True):
-            return type(True)
-        else:
-            sys.exit("Type error")
-
-    def visit_or(self, exp, env):
-        """
-        Usage:
-            >>> e = Or(Bln(True), Bln(False))
-            >>> ev = TypeCheckVisitor()
-            >>> e.accept(ev, None)
-            <class 'bool'>
-        """
-        if exp.left.accept(self, env) == exp.right.accept(self, env) == type(True):
-            return type(True)
-        else:
-            sys.exit("Type error")
-
-    def visit_add(self, exp, env):
-        """
-        Usage:
-            >>> e = Add(Num(1), Num(2))
-            >>> ev = TypeCheckVisitor()
-            >>> e.accept(ev, None)
-            <class 'int'>
-        """
-        if exp.left.accept(self, env) == exp.right.accept(self, env) == type(1):
-            return type(1)
-        else:
-            sys.exit("Type error")
-
-    def visit_sub(self, exp, env):
-        """
-        Usage:
-            >>> e = Sub(Num(1), Num(2))
-            >>> ev = TypeCheckVisitor()
-            >>> e.accept(ev, None)
-            <class 'int'>
-        """
-        if exp.left.accept(self, env) == exp.right.accept(self, env) == type(1):
-            return type(1)
-        else:
-            sys.exit("Type error")
-
-    def visit_mul(self, exp, env):
-        """
-        Usage:
-            >>> e = Mul(Num(1), Num(2))
-            >>> ev = TypeCheckVisitor()
-            >>> e.accept(ev, None)
-            <class 'int'>
-        """
-        if exp.left.accept(self, env) == exp.right.accept(self, env) == type(1):
-            return type(1)
-        else:
-            sys.exit("Type error")
-
-    def visit_div(self, exp, env):
-        """
-        Usage:
-            >>> e = Div(Num(1), Num(0))
-            >>> ev = TypeCheckVisitor()
-            >>> e.accept(ev, None)
-            <class 'int'>
-        """
-        if exp.left.accept(self, env) == exp.right.accept(self, env) == type(1):
-            return type(1)
-        else:
-            sys.exit("Type error")
-
-    def visit_leq(self, exp, env):
-        """
-        Usage:
-            >>> e = Leq(Num(1), Num(0))
-            >>> ev = TypeCheckVisitor()
-            >>> e.accept(ev, None)
-            <class 'bool'>
-        """
-        if exp.left.accept(self, env) == exp.right.accept(self, env) == type(1):
-            return type(True)
-        else:
-            sys.exit("Type error")
-
-    def visit_lth(self, exp, env):
-        """
-        Usage:
-            >>> e = Lth(Num(1), Num(0))
-            >>> ev = TypeCheckVisitor()
-            >>> e.accept(ev, None)
-            <class 'bool'>
-        """
-        if exp.left.accept(self, env) == exp.right.accept(self, env) == type(1):
-            return type(True)
-        else:
-            sys.exit("Type error")
-
-    def visit_neg(self, exp, env):
-        """
-        Usage:
-            >>> e = Neg(Num(1))
-            >>> ev = TypeCheckVisitor()
-            >>> e.accept(ev, None)
-            <class 'int'>
-        """
-        if exp.exp.accept(self, env) == type(1):
-            return type(1)
-        else:
-            sys.exit("Type error")
-
-    def visit_not(self, exp, env):
-        """
-        Usage:
-            >>> e = Not(Bln(False))
-            >>> ev = TypeCheckVisitor()
-            >>> e.accept(ev, None)
-            <class 'bool'>
-        """
-        if exp.exp.accept(self, env) == type(True):
-            return type(True)
-        else:
-            sys.exit("Type error")
-
-    def visit_ifThenElse(self, exp, env):
-        """
-        Usage:
-            >>> e0 = Lth(Num(1), Num(0))
-            >>> e = IfThenElse(e0, Bln(True), e0)
-            >>> ev = TypeCheckVisitor()
-            >>> e.accept(ev, {})
-            <class 'bool'>
-
-            >>> e0 = Lth(Num(1), Num(0))
-            >>> e = IfThenElse(e0, Var('v'), Var('w'))
-            >>> ev = TypeCheckVisitor()
-            >>> tp0 = ArrowType(type(1), type(2))
-            >>> tp1 = ArrowType(type(3), type(4))
-            >>> e.accept(ev, {'v':tp0, 'w':tp1})
-            <class 'int'> -> <class 'int'>
-        """
-        type_cond = exp.cond.accept(self, env)
-        type_e0 = exp.e0.accept(self, env)
-        type_e1 = exp.e1.accept(self, env)
-
-        if type_e0 == type_e1 and type_cond == type(True):
-            return type_e0
-        else:
-            sys.exit("Type error")
-
-    def visit_let(self, exp, env):
-        """
-        Usage:
-            >>> e = Let('v', type(True), Not(Bln(False)), Var('v'))
-            >>> ev = TypeCheckVisitor()
-            >>> e.accept(ev, {})
-            <class 'bool'>
-
-            >>> e = Let('v', type(1), Num(2), Add(Var('v'), Num(3)))
-            >>> ev = TypeCheckVisitor()
-            >>> e.accept(ev, {})
-            <class 'int'>
-        """
-        e0_type = exp.exp_def.accept(self, env)
-
-        if exp.tp_var == e0_type:
-            new_env = dict(env)
-            new_env[exp.identifier] = e0_type
-
-            return exp.exp_body.accept(self, new_env)
-        else:
-            sys.exit("Def error")
-
-    def visit_fn(self, exp, env):
-        """
-        Usage:
-            >>> e = Fn('v', type(True), Var('v'))
-            >>> ev = TypeCheckVisitor()
-            >>> e.accept(ev, {})
-            <class 'bool'> -> <class 'bool'>
-
-            >>> e = Fn('v', type(1), Add(Var('v'), Num(3)))
-            >>> ev = TypeCheckVisitor()
-            >>> e.accept(ev, {})
-            <class 'int'> -> <class 'int'>
-
-            >>> e0 = Fn('y', type(1), Add(Var('y'), Var('x')))
-            >>> e1 = Fn('x', type(1), e0)
-            >>> ev = TypeCheckVisitor()
-            >>> e1.accept(ev, {})
-            <class 'int'> -> ( <class 'int'> -> <class 'int'> )
-        """
-        new_env = dict(env)
-        new_env[exp.formal] = exp.tp_var
-
-        body_type = exp.body.accept(self, new_env)
-
-        return ArrowType(exp.tp_var, body_type)
-    
-    def visit_app(self, exp, env):
-        """
-        Usage:
-            >>> e0 = Fn('v', type(1), Add(Var('v'), Num(3)))
-            >>> e1 = App(e0, Num(1))
-            >>> ev = TypeCheckVisitor()
-            >>> e1.accept(ev, {})
-            <class 'int'>
-
-            >>> e0 = Fn('y', type(1), Add(Var('y'), Var('x')))
-            >>> e1 = Fn('x', type(1), e0)
-            >>> e2 = App(e1, Num(1))
-            >>> ev = TypeCheckVisitor()
-            >>> e2.accept(ev, {})
-            <class 'int'> -> <class 'int'>
-        """
-        function_type = exp.function.accept(self, env)
-        actual_type = exp.actual.accept(self, env)
-        if actual_type == function_type.hd:
-            return function_type.tl
-        else:
-            sys.exit("Type error")
+    def visit_let(self, exp, prog):
+        init_value = exp.exp_def.accept(self, prog)
+        prog.add_inst(AsmModule.Add(exp.identifier, init_value, "x0"))
+        body_value = exp.exp_body.accept(self, prog)
+        return body_value
